aaf16c90adf18eefcf4940922095cd48b9717f73
inzagher <IvanovDV87@yandex.ru>
2022-02-10 15:02:06 +0300

    Create server project

---
+++ inzagher-camunda-server/src/main/java/inzagher/camunda/server/CamundaServerApplication.java
@@ -0,0 +1,11 @@
+package inzagher.camunda.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaServerApplication.class, args);
+	}
+}
